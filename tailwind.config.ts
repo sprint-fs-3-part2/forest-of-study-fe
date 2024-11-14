2df12519bbbc679d56dd66cbe30526f667324bbb
heonq <gkagjsrb@naver.com>
2024-11-14 09:32:57 +0900

    feat: CommonBtn 컴포넌트 작성

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      screens: {
+        sm: '375px',
+        md: '744px',
+        xl: '1200px',
       },
       fontFamily: {
-        sans: ["var(--font-pretendard-variable)"],
-        pretendard: ["var(--font-pretendard-variable)"],
-        jejudoldam: ["var(--font-jejudoldam)"],
+        sans: ['var(--font-pretendard-variable)'],
+        pretendard: ['var(--font-pretendard-variable)'],
+        jejudoldam: ['var(--font-jejudoldam)'],
       },
     },
   },
