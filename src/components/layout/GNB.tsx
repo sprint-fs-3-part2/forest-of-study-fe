34fdbbffdf962d21794f90fd86d6599df0e1c40b
heonq <gkagjsrb@naver.com>
2024-11-17 16:24:28 +0900

    ✨ feat: GNB 컴포넌트 구현

---
+++ src/components/layout/GNB.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import Logo from '@/public/img_logo.svg';
+import { CommonBtn } from '@/components/common/CommonBtn';
+import cn from '@/lib/cn';
+import { usePathname } from 'next/navigation';
+
+export function GNB() {
+  const pathname = usePathname();
+
+  return (
+    <nav
+      className={cn(
+        'w-full',
+        'flex justify-between',
+        'px-[16px] md:px-[24px] xl:px-[220px]',
+        'py-[31px] md:py-[21px] xl:py-[21px]',
+      )}
+    >
+      <Image
+        className={cn('w-[106px] md:w-[181px] xl:w-[181px]')}
+        src={Logo}
+        alt='로고 이미지'
+      />
+      {pathname === '/' ? (
+        <CommonBtn
+          widthType='dynamic'
+          heightType='dynamic'
+        >
+          스터디 만들기
+        </CommonBtn>
+      ) : null}
+    </nav>
+  );
+}
