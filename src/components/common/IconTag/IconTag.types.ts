2d05dbcd763d45f7f76c2a2079f6ade015325a86
heonq <gkagjsrb@naver.com>
2024-11-17 09:19:54 +0900

    ♻️ reafctor:불필요한 타입 제거

--- src/components/common/IconTag/IconTag.types.ts
+++ src/components/common/IconTag/IconTag.types.ts
@@ -1,20 +1,4 @@
 import { StaticImport } from 'next/dist/shared/lib/get-img-props';
-import React from 'react';
-
-type IconTagVariant = 'point' | 'reaction' | 'addReaction';
-type IconTagParentComponent = 'page' | 'card';
-type IconTagParentBackground = 'photo' | 'color';
-type IconTagOnClick = () => void;
-
-export interface IconTagProps2 {
-  variant: IconTagVariant;
-  parentComponent: IconTagParentComponent;
-  parentBackground?: IconTagParentBackground;
-  onClick?: IconTagOnClick;
-  point?: number;
-  reaction?: string;
-  reactionCount?: number;
-}
 
 export interface IconTagProps {
   variant: 'point' | 'reaction';
