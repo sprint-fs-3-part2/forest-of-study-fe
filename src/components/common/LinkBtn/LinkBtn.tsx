a286a183fc3c185ab8729da73781c8933efe3ac5
heonq <gkagjsrb@naver.com>
2024-11-19 18:01:57 +0900

    💄 style(LinkBtn): 반응형 디자인 적용

--- src/components/common/LinkBtn/LinkBtn.tsx
+++ src/components/common/LinkBtn/LinkBtn.tsx
@@ -3,13 +3,21 @@ import ArrowRightIcon from '@/public/icons/ic_arrow_right.png';
 import { LinkBtnProps } from './LinkBtn.types';
 import cn from '@/lib/cn';
 
+const PADDING_LEFT = 'pl-[10px] md:pl-[24px] xl:pl-[24px]';
+const PADDING_RIGHT = 'pr-[6px] md:pr-[16px] xl:pr-[16px]';
+const PADDING_Y = ' py-[8px] md:py-[12px] xl:py-[12px]';
+
 export function LinkBtn({ children, className, ...props }: LinkBtnProps) {
   const ICON_SIZE = 24;
 
   return (
     <button
       className={cn(
-        'flex justify-center items-center py-[12px] pl-[24px] pr-[16px] border border-gray-light rounded-[15px] text-gray gap-[4px]',
+        'flex justify-center items-center',
+        'border border-gray-light rounded-[15px] text-gray gap-[4px]',
+        PADDING_LEFT,
+        PADDING_RIGHT,
+        PADDING_Y,
         className,
       )}
       {...props}
