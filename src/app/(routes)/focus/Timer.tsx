76b909083122006de613fdcc5d4b5dd09a020b55
Daewony <alexnori.com@gmail.com>
2024-11-19 18:38:56 +0900

    ✨ feat: 집중 중단 및 포인트 획득 알림 메시지 추가

--- src/app/(routes)/focus/Timer.tsx
+++ src/app/(routes)/focus/Timer.tsx
@@ -8,92 +8,142 @@ import playIcon from '@/public/icons/play_icon.png'; // 시작
 import stopIcon from '@/public/icons/stop_icon.png'; // 정지
 import pauseIcon from '@/public/icons/pause_icon.png'; // 일시정지
 import restartIcon from '@/public/icons/restart_icon.png'; // 재시작
+import timerIcon from '@/public/icons/timer_icon.png'; // 타이머
 
-type TimerProps = {
-  initialTime?: number;
-};
+import { IconTag } from '@/components/common/IconTag';
 
-const Timer = ({ initialTime = 25 }: TimerProps) => {
-  const initTime = initialTime * 60; // 25분
+const Timer = () => {
+  const initialTime = 25;
+  const oneMinute = 60;
+  const initTime = initialTime * oneMinute; // 25분
   const {
     secondsLeft,
     isPaused,
     formattedMinutes,
     formattedSeconds,
+    point,
+    showGetPointNotification,
     start,
     pause,
     reset,
+    finish,
   } = useTimer(initTime);
 
   return (
-    <div className='border border-gray-light rounded-[20px] flex flex-col justify-around items-center h-[546px] max-md:h-[510px] max-sm:h-[358px]'>
-      {/* 타이머 제목 */}
-      <h2 className='font-extrabold text-2xl'>오늘의 집중</h2>
-
-      {/* 타이머 */}
-      {secondsLeft >= 10 && (
-        <span className='font-extrabold xl:text-[150px] md:text-[120px] sm:text-[80px] max-sm:text-[80px]'>
-          {formattedMinutes + ':' + formattedSeconds}
-        </span>
-      )}
-      {secondsLeft < 10 && secondsLeft >= 0 && (
-        <span className='font-extrabold text-red xl:text-[150px] md:text-[120px] sm:text-[80px]'>
-          {formattedMinutes + ':' + formattedSeconds}
-        </span>
-      )}
-      {secondsLeft < 0 && (
-        <span className='font-extrabold text-gray xl:text-[150px] md:text-[120px] sm:text-[80px]'>
-          {formattedMinutes + ':' + formattedSeconds}
-        </span>
-      )}
-
-      {/* 타이머 관련 버튼 */}
-      <div className='flex justify-between gap-6'>
-        {secondsLeft === initTime && (
-          <ActionBtn
-            variant={'ellipse'}
-            icon={playIcon}
-            disabled={false}
-            onClick={start}
-          >
-            Start!
-          </ActionBtn>
-        )}
-        {secondsLeft !== initTime && secondsLeft >= 0 && (
-          <>
-            <ActionBtn
-              variant={'circle'}
-              icon={pauseIcon}
-              disabled={isPaused}
-              onClick={isPaused ? undefined : pause}
+    <>
+      <div className='border border-gray-light rounded-[20px] flex flex-col justify-evenly items-center h-[546px] max-md:h-[510px] max-sm:h-[358px] relative'>
+        {/* 타이머 제목 */}
+        <div className='flex flex-col items-center gap-6'>
+          <h2 className='font-extrabold text-2xl'>오늘의 집중</h2>
+          {secondsLeft !== initTime && (
+            <IconTag
+              fillColor='white'
+              parentComponent='card'
+              icon={timerIcon}
+              text={`${initialTime}:00`}
+              variant='point'
             />
+          )}
+        </div>
+
+        {/* 타이머 */}
+        {secondsLeft >= 10 && (
+          <span className='font-extrabold xl:text-[150px] md:text-[120px] sm:text-[80px] max-sm:text-[80px]'>
+            {formattedMinutes + ':' + formattedSeconds}
+          </span>
+        )}
+        {secondsLeft < 10 && secondsLeft >= 0 && (
+          <span className='font-extrabold text-red xl:text-[150px] md:text-[120px] sm:text-[80px]'>
+            {formattedMinutes + ':' + formattedSeconds}
+          </span>
+        )}
+        {secondsLeft < 0 && (
+          <span className='font-extrabold text-gray xl:text-[150px] md:text-[120px] sm:text-[80px]'>
+            {formattedMinutes + ':' + formattedSeconds}
+          </span>
+        )}
+
+        {/* 타이머 관련 버튼 */}
+        <div className='flex justify-between gap-6'>
+          {secondsLeft === initTime && (
             <ActionBtn
               variant={'ellipse'}
               icon={playIcon}
-              disabled={!isPaused}
-              onClick={isPaused ? start : undefined}
+              disabled={false}
+              onClick={start}
             >
               Start!
             </ActionBtn>
+          )}
+          {secondsLeft !== initTime && secondsLeft >= 0 && (
+            <>
+              <ActionBtn
+                variant={'circle'}
+                icon={pauseIcon}
+                disabled={isPaused}
+                onClick={isPaused ? undefined : pause}
+              />
+              <ActionBtn
+                variant={'ellipse'}
+                icon={playIcon}
+                disabled={!isPaused}
+                onClick={isPaused ? start : undefined}
+              >
+                Start!
+              </ActionBtn>
+              <ActionBtn
+                variant={'circle'}
+                icon={restartIcon}
+                disabled={!isPaused}
+                onClick={reset}
+              />
+            </>
+          )}
+          {secondsLeft < 0 && (
             <ActionBtn
-              variant={'circle'}
-              icon={restartIcon}
-              disabled={!isPaused}
-              onClick={reset}
-            />
-          </>
-        )}
-        {secondsLeft < 0 && (
-          <ActionBtn
-            variant={'ellipse'}
-            icon={stopIcon}
-            disabled={false}
-            onClick={reset}
+              variant={'ellipse'}
+              icon={stopIcon}
+              disabled={false}
+              onClick={finish}
+            >
+              Stop!
+            </ActionBtn>
+          )}
+        </div>
+        {/* 알림 */}
+        {initTime !== secondsLeft && isPaused && (
+          <Notification
+            bgColor='pink'
+            textColor='red'
           >
-            Stop!
-          </ActionBtn>
+            🚨 집중이 중단되었습니다
+          </Notification>
+        )}
+        {showGetPointNotification && (
+          <Notification
+            bgColor='green'
+            textColor='green-text'
+          >
+            🎉 {point}포인트를 획득했습니다!
+          </Notification>
         )}
       </div>
+    </>
+  );
+};
+
+type NotificationProps = {
+  bgColor: string;
+  textColor: string;
+  children: React.ReactNode;
+};
+
+const Notification = ({ bgColor, textColor, children }: NotificationProps) => {
+  return (
+    <div
+      className={`absolute bottom-[-120px] px-7 py-3 bg-${bgColor} text-${textColor} font-medium rounded-xl`}
+    >
+      {children}
     </div>
   );
 };
--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -2,18 +2,24 @@ import { formatTimeForTimer } from '@/utils/formatTimeForTimer';
 import { useEffect, useRef, useState } from 'react';
 
 type TimerReturnType = {
-  secondsLeft: number;
-  isPaused: boolean;
-  formattedMinutes: string;
-  formattedSeconds: string;
-  start: () => void;
-  pause: () => void;
-  reset: () => void;
+  secondsLeft: number; // 남은 시간
+  isPaused: boolean; // 일시정지 상태
+  formattedMinutes: string; // 포맷팅된 분
+  formattedSeconds: string; // 포맷팅된 초
+  point: number; // 포인트
+  showGetPointNotification: boolean; // 포인트 획득 알림창 표시 여부
+  start: () => void; // 타이머 시작
+  pause: () => void; // 타이머 일시정지
+  reset: () => void; // 타이머 초기화
+  finish: () => void; // 타이머 완료(포인트 획득 요청 및 초기화)
 };
 
 const useTimer = (initialTime: number): TimerReturnType => {
   const [isPaused, setIsPaused] = useState<boolean>(true); // 일시정지 상태
   const [secondsLeft, setSecondsLeft] = useState<number>(initialTime); // 남은 시간
+  const [showGetPointNotification, setGetPointShowNotification] =
+    useState<boolean>(false); // 포인트 획득 알림창 표시 여부
+  const [point, setPoint] = useState<number>(0);
 
   const secondsLeftRef = useRef<number>(secondsLeft);
   const isPausedRef = useRef<boolean>(isPaused);
@@ -36,12 +42,35 @@ const useTimer = (initialTime: number): TimerReturnType => {
     isPausedRef.current = true;
   };
 
-  // 초기화 기능
+  // 타이머 초기화
   const reset = () => {
     setIsPaused(true);
     isPausedRef.current = true;
     setSecondsLeft(initialTime);
     secondsLeftRef.current = initialTime;
+  };
+
+  // 타이머 완료 버튼 클릭 시 초기화
+  const finish = () => {
+    // 합산 포인트 요청
+    const passedTime = initialTime - secondsLeftRef.current;
+    const oneMinute = 60;
+    const tenMinutes = oneMinute * 10;
+    const tenMinutesGetPoint = Math.floor(passedTime / tenMinutes);
+    const finishPoint = 3;
+    const totalPoint = tenMinutesGetPoint * finishPoint;
+    setPoint(totalPoint);
+
+    // 알림창 표시
+    setGetPointShowNotification(true);
+
+    // 3초 후 알림창 숨김
+    setTimeout(() => {
+      setGetPointShowNotification(false);
+    }, 3000);
+
+    // 타이머 초기화
+    reset();
   };
 
   useEffect(() => {
@@ -65,9 +94,12 @@ const useTimer = (initialTime: number): TimerReturnType => {
     isPaused,
     formattedMinutes,
     formattedSeconds,
+    point,
+    showGetPointNotification,
     start,
     pause,
     reset,
+    finish,
   };
 };
 
