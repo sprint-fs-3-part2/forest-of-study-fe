c1eb18e90d623765d1695440dc4ea972301bb9e2
ChoboDev <gealot@gmail.com>
2024-11-20 17:17:46 +0900

    🗃️ Chore(codegen): update swagger types
    백엔드에 추가된 스웨거 타입을 업데이트

--- src/lib/types/api/data-contracts.ts
+++ src/lib/types/api/data-contracts.ts
@@ -9,9 +9,138 @@
  * ---------------------------------------------------------------
  */
 
-export type CreateStudyDto = object;
+export interface CreateStudyDto {
+  /**
+   * 스터디 이름
+   * @example "UX 스터디"
+   */
+  name: string;
+  /**
+   * 스터디 개설자의 닉네임
+   * @example "K.K."
+   */
+  nickname: string;
+  /**
+   * 스터디 소개
+   * @example "나비보벳따우"
+   */
+  intro: string;
+  /**
+   * 스터디 배경 이미지
+   * @example "green"
+   */
+  background: string;
+  /**
+   * 스터디 비밀번호
+   * @minLength 8
+   * @maxLength 64
+   * @example "1q2w3e4r"
+   */
+  password: string;
+}
 
-export type UpdateStudyDto = object;
+export interface CreateStudyResponseDto {
+  /**
+   * 스터디 ID (UUIDv4)
+   * @example "64aa3ca4-65ed-460d-b100-7fc93829a07d"
+   */
+  id: string;
+}
+
+export interface RecentStudiesRequestDto {
+  /**
+   * 조회할 스터디 UUID(v4) 배열(0~3개)
+   * @maxItems 3
+   * @minItems 0
+   * @uniqueItems true
+   * @example ["f47ac10b-58cc-4372-a567-0e02b2c3d479","550e8400-e29b-41d4-a716-446655440000"]
+   */
+  uuids: string[];
+}
+
+export interface RecentStudiesResponseDto {
+  /**
+   * 스터디 이름
+   * @example "UX 스터디"
+   */
+  name: string;
+  /**
+   * 스터디 개설자의 닉네임
+   * @example "K.K."
+   */
+  nickname: string;
+  /**
+   * 스터디 소개
+   * @example "나비보벳따우"
+   */
+  intro: string;
+  /**
+   * 스터디 배경 이미지
+   * @example "green"
+   */
+  background: string;
+}
+
+export interface SearchKeywordResponseDto {
+  /**
+   * 스터디 이름
+   * @example "UX 스터디"
+   */
+  name: string;
+  /**
+   * 스터디 개설자의 닉네임
+   * @example "K.K."
+   */
+  nickname: string;
+  /**
+   * 스터디 소개
+   * @example "나비보벳따우"
+   */
+  intro: string;
+  /**
+   * 스터디 배경 이미지
+   * @example "green"
+   */
+  background: string;
+}
+
+export interface UpdateStudyDto {
+  /**
+   * 스터디 이름
+   * @example "UX 스터디"
+   */
+  name?: string;
+  /**
+   * 스터디 개설자의 닉네임
+   * @example "K.K."
+   */
+  nickname?: string;
+  /**
+   * 스터디 소개
+   * @example "나비보벳따우"
+   */
+  intro?: string;
+  /**
+   * 스터디 배경 이미지
+   * @example "green"
+   */
+  background?: string;
+  /**
+   * 스터디 비밀번호
+   * @minLength 8
+   * @maxLength 64
+   * @example "1q2w3e4r"
+   */
+  password?: string;
+}
+
+export interface UpdateStudyResponseDto {
+  /**
+   * 스터디 ID (UUIDv4)
+   * @example "64aa3ca4-65ed-460d-b100-7fc93829a07d"
+   */
+  id?: string;
+}
 
 export type CreateHabitDto = object;
 
@@ -27,15 +156,19 @@ export type UpdateReactionDto = object;
 
 export type AppControllerGetHelloData = any;
 
-export type StudiesControllerCreateData = any;
+export type StudiesControllerCreateStudyData = CreateStudyResponseDto;
 
-export type StudiesControllerFindAllData = any;
+export type StudiesControllerGetStudiesData = any;
 
-export type StudiesControllerFindOneData = any;
+export type StudiesControllerGetRecentStudiesData = RecentStudiesResponseDto;
 
-export type StudiesControllerUpdateData = any;
+export type StudiesControllerSearchStudiesData = SearchKeywordResponseDto;
 
-export type StudiesControllerRemoveData = any;
+export type StudiesControllerGetStudyByIdData = any;
+
+export type StudiesControllerUpdateStudyData = UpdateStudyResponseDto;
+
+export type StudiesControllerDeleteStudyData = any;
 
 export type HabitsControllerCreateData = any;
 
