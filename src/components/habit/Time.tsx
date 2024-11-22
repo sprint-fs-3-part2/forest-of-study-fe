d53dd91e66496cdccb63dfb069c0ea9079bbe06d
heonq <gkagjsrb@naver.com>
2024-11-22 11:08:56 +0900

    💄 style: 시간 컴포넌트 스타일 수정

--- src/components/habit/Time.tsx
+++ src/components/habit/Time.tsx
@@ -5,7 +5,7 @@ export const Time = () => {
   return (
     <>
       <div className='mb-2'>현재 시간</div>
-      <div className='w-[184px] py-2 px-3 border rounded-[50px] mb-[24px]'>
+      <div className='w-fit py-2 px-3 border rounded-[50px] mb-[24px]'>
         {time}
       </div>
     </>
