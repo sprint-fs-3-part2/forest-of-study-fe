335165973ff31e2ac5f3ac3d56c9872509dc171e
heonq <gkagjsrb@naver.com>
2024-11-22 11:48:14 +0900

    ♻️ refactor: axios interceptor 설정

--- src/lib/axios/axiosInstance.ts
+++ src/lib/axios/axiosInstance.ts
@@ -15,6 +15,36 @@ const getBaseConfig = (): AxiosRequestConfig => {
 
 const createAxiosInstance = () => {
   const config = getBaseConfig();
-  return axios.create(config);
+  const axiosInstance = axios.create(config);
+
+  axiosInstance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (error.response) {
+        switch (error.response.status) {
+          case 401:
+            console.error('인증 에러');
+            break;
+          case 404:
+            console.error('리소스를 찾을 수 없습니다');
+            break;
+          case 500:
+            console.error('서버 에러');
+            break;
+          default:
+            console.error('API 요청 중 에러 발생:', error);
+        }
+      } else if (error.request) {
+        console.error('네트워크 에러');
+      }
+
+      return Promise.reject(error);
+    },
+  );
+
+  return axiosInstance;
 };
+
 export const axiosInstance = createAxiosInstance();
