9e5035bd55092248a6d62b0c2820829dcc88d11e
heonq <gkagjsrb@naver.com>
2024-11-26 21:02:42 +0900

    💄 style: Actions 컴포넌트 스타일 수정

--- src/components/studyPage/Actions.tsx
+++ src/components/studyPage/Actions.tsx
@@ -3,16 +3,21 @@ import { ActionsProps } from './studyPage.types';
 export const Actions = ({ setVariant }: ActionsProps) => {
   return (
     <div className='flex gap-2 md:gap-4 xl:gap-4 text-[12px] md:text-[16px] xl:text-[16px]'>
-      <span className='text-green-text'>공유하기</span>
+      <span className='text-green-text cursor-pointer'>공유하기</span>
       <span>|</span>
       <span
-        className='text-green-text'
+        className='text-green-text cursor-pointer'
         onClick={() => setVariant('edit')}
       >
         수정하기
       </span>
       <span>|</span>
-      <span onClick={() => setVariant('delete')}>스터디 삭제하기</span>
+      <span
+        className='cursor-pointer'
+        onClick={() => setVariant('delete')}
+      >
+        스터디 삭제하기
+      </span>
     </div>
   );
 };
