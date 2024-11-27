c6a28207b58bbeed76aae435c63d3129416ca8f0
KJY <139238536+dani784601@users.noreply.github.com>
2024-11-27 09:09:01 +0900

    Fix(store): add window check for localStorage access

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,11 +1,15 @@
 const Store = {
   getItem(key: string) {
-    const response = localStorage.getItem(key);
-    return response && JSON.parse(response);
+    if (typeof window !== 'undefined') {
+      const response = localStorage.getItem(key);
+      return response && JSON.parse(response);
+    }
   },
   setItem(key: string, item: any) {
-    const data = JSON.stringify(item);
-    localStorage.setItem(key, data);
+    if (typeof window !== 'undefined') {
+      const data = JSON.stringify(item);
+      localStorage.setItem(key, data);
+    }
   },
 };
 
