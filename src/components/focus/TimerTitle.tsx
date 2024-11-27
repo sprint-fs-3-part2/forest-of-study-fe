415aa63f2f6ecb7290e0f8b08c6e20deb8d13aba
Daewony <alexnori.com@gmail.com>
2024-11-27 09:46:49 +0900

    fix: 타이머 초기 시간 오류 수정

--- src/components/focus/TimerTitle.tsx
+++ src/components/focus/TimerTitle.tsx
@@ -3,17 +3,13 @@ import timerIcon from '@/public/icons/timer_icon.png'; // 타이머
 import { IconTag } from '@/components/common/IconTag';
 
 type TimerTitleProps = {
-  minutesPerSession: number;
   sessionTimeInSeconds: number;
   secondsLeft: number;
 };
 
-const TimerTitle = ({
-  minutesPerSession,
-  sessionTimeInSeconds,
-  secondsLeft,
-}: TimerTitleProps) => {
+const TimerTitle = ({ sessionTimeInSeconds, secondsLeft }: TimerTitleProps) => {
   const isTimerRunning = secondsLeft !== sessionTimeInSeconds;
+
   return (
     <div className='flex flex-col items-center gap-6'>
       <h2 className='font-extrabold text-2xl'>오늘의 집중</h2>
@@ -22,7 +18,7 @@ const TimerTitle = ({
           fillColor='white'
           parentComponent='card'
           icon={timerIcon}
-          text={`${minutesPerSession}:00`}
+          text={formatSecondsToMinutesSeconds(sessionTimeInSeconds)}
           variant='point'
         />
       )}
@@ -31,3 +27,13 @@ const TimerTitle = ({
 };
 
 export default TimerTitle;
+
+const formatSecondsToMinutesSeconds = (totalSeconds: number): string => {
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  const formattedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+};
