3613b1511ede2c71306c88f2f915ac48908ab82b
KJY <139238536+dani784601@users.noreply.github.com>
2024-11-28 11:50:00 +0900

    ✨ Feat: Add NotFound component for handling 404 errors

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <div className='text-2xl font-bold'>이페이지는 존재하지 않습니다</div>
+    </div>
+  );
+}
